7271d854056dae33627da2b5ba79fe512eb54d47
김용민 <dydals3440@gmail.com>
2024-10-21 03:17:27 +0900

    feat: Account 컴포넌트 제작

--- src/components/home/Account.tsx
+++ src/components/home/Account.tsx
@@ -1,1 +1,61 @@
-function Account() {}
+import Flex from '@shared/Flex'
+import Text from '@shared/Text'
+import Button from '@shared/Button'
+import Spacing from '@shared/Spacing'
+import Image from 'next/image'
+
+function Account() {
+  const hasAccount = false
+
+  if (hasAccount) {
+    return (
+      <div style={{ padding: 24 }}>
+        <Flex justify="space-between" align="center">
+          <Flex direction="column">
+            <Text typography="t6" color="gray600">
+              매튜 회원님의 자신
+            </Text>
+            <Spacing size={2} />
+            <Text typography="t3" bold>
+              7,000원
+            </Text>
+          </Flex>
+          <Button>분석</Button>
+        </Flex>
+      </div>
+    )
+  }
+
+  // 계좌를 보유하고 있지 않을 떄
+  // 계좌를 개설중일수도 있음.
+  // READY | DONE
+  const 계좌개설상태 = 'READY'
+  const title =
+    계좌개설상태 === 'READY'
+      ? '만들고 있으신\n계좌가 있으시군요'
+      : '계좌 개설이\n더 쉽고 빨라졌어요'
+  const buttonLabel =
+    계좌개설상태 === 'READY' ? '이어만들기' : '3분만에 개설하기'
+  return (
+    <div style={{ padding: 24 }}>
+      <Flex justify="space-between">
+        <Flex direction="column">
+          {/* pre-wrap -> 제대로 줄바꿈 됨 */}
+          <Text bold style={{ whiteSpace: 'pre-wrap' }}>
+            {title}
+          </Text>
+          <Spacing size={8} />
+          <Button>{buttonLabel}</Button>
+        </Flex>
+        <Image
+          alt=""
+          width={80}
+          height={80}
+          src="https://cdn2.iconfinder.com/data/icons/scenarium-vol-3-1/128/002_money_cash_gold_wealth_income_deposit_finance-128.png"
+        />
+      </Flex>
+    </div>
+  )
+}
+
+export default Account
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import dynamic from 'next/dynamic'
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
-import Skeleton from '@shared/Skeleton'
+
+import Account from '@components/home/Account'
+import { BannerSkeleton } from '@components/home/EventBanners'
 
 // lazy + suspense in react
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   // loading: () => <div>Loading...</div>,
-  loading: () => (
-    <Skeleton width="100%" height={100} style={{ borderRadius: 8 }} />
-  ),
+  loading: () => <BannerSkeleton />,
   // 해당 컴포넌트를 SSR 할껀지 말껀지, 유저에게 빨리보여줘야하는 데이터는 아님 Banner는
   ssr: false,
 })
@@ -16,18 +14,8 @@ const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
 export default function Home() {
   return (
     <>
-      <Container>
-        <EventBanners />
-        <h1 css={bold}>Hello</h1>
-      </Container>
+      <EventBanners />
+      <Account />
     </>
   )
 }
-
-const Container = styled.div`
-  background-color: pink;
-`
-
-const bold = css`
-  font-weight: bold;
-`
