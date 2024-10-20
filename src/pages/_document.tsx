1cdd5e367fa54fd181c82bcb8764c52f944f2408
김용민 <dydals3440@gmail.com>
2024-10-21 02:21:24 +0900

    first commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
+// SSR 에서만 렌더링, CSR 에서는 렌더링 X
+// 기본 HTML 설정을 여기서 함.
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
