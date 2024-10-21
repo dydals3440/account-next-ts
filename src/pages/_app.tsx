f2fe8b3e6feef30cc9819f87cdd0766efade9c50
김용민 <dydals3440@gmail.com>
2024-10-21 20:25:13 +0900

    feat: CardList Page Infinite Scroll with Dehydrate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { Global } from '@emotion/react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query'
 
 import globalStyles from '@styles/globalStyles'
 import Layout from '@shared/Layout'
@@ -12,17 +12,21 @@ const client = new QueryClient({})
 
 export default function App({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
   return (
     <Layout>
       <Global styles={globalStyles} />
       <SessionProvider session={session}>
         <QueryClientProvider client={client}>
-          <AuthGuard>
-            <Navbar />
-            <Component {...pageProps} />
-          </AuthGuard>
+          {/* 서버측에서 수행한 값을, 클라이언트에도 그대로 복원해서 사용 가능.*/}
+          {/* 이미 액션 호출이 일어났고, 키값으로 저장되어 있어서, 클라에서는 같은 키값으로 접근하면, 데이터가 바로 채워진 상태로 사용 가능 */}
+          <Hydrate state={dehydratedState}>
+            <AuthGuard>
+              <Navbar />
+              <Component {...pageProps} />
+            </AuthGuard>
+          </Hydrate>
         </QueryClientProvider>
       </SessionProvider>
     </Layout>
--- src/pages/card/index.tsx
+++ src/pages/card/index.tsx
@@ -1,5 +1,83 @@
+import { QueryClient, dehydrate, useInfiniteQuery } from 'react-query'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+import getCards from '@remote/card'
+import ListRow from '@shared/ListRow'
+import { useCallback } from 'react'
+import Badge from '@shared/Badge'
+import { useRouter } from 'next/router'
+
 function CardListPage() {
-  return <div>CardDetailPage</div>
+  const router = useRouter()
+  const {
+    data,
+    hasNextPage = false,
+    fetchNextPage,
+    isFetching,
+  } = useInfiniteQuery(['cards'], ({ pageParam }) => getCards(pageParam), {
+    getNextPageParam: (snapshot) => {
+      return snapshot.lastVisible
+    },
+  })
+
+  const loadMore = useCallback(() => {
+    if (hasNextPage === false || isFetching) {
+      return
+    }
+    fetchNextPage()
+  }, [hasNextPage, fetchNextPage, isFetching])
+
+  if (data == null) {
+    return null
+  }
+
+  // 페이징을 하기에 data를 바로 못씀 플렛하게 펼쳐야함.
+  const cards = data?.pages.map(({ items }) => items).flat()
+
+  return (
+    <div>
+      <InfiniteScroll
+        dataLength={cards.length}
+        hasMore={hasNextPage}
+        loader={<ListRow.Skeleton />}
+        next={loadMore}
+        scrollThreshold="100px"
+      >
+        <ul>
+          {cards.map((card, index) => (
+            <ListRow
+              key={card.id}
+              contents={
+                <ListRow.Texts title={`${index + 1}위`} subTitle={card.name} />
+              }
+              right={
+                card.payback != null ? <Badge label={card.payback} /> : null
+              }
+              withArrow={true}
+              onClick={() => {
+                router.push(`/card/${card.id}`)
+              }}
+            />
+          ))}
+        </ul>
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+// SSR이 먼저 실행되고 _app 이 그려짐.
+export async function getServerSideProps() {
+  console.log('SSR')
+
+  const client = new QueryClient()
+
+  await client.prefetchInfiniteQuery(['cards'], () => getCards())
+
+  return {
+    props: {
+      dehydratedState: JSON.parse(JSON.stringify(dehydrate(client))),
+    },
+  }
 }
 
 export default CardListPage
